ba0d0521689bc4511b73ff3a870bc778529b91da
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-08-05 23:18:37 +0530

    item component and submit function

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,28 @@
 import React,{useState} from "react";
 import { Container, Table } from "react-bootstrap";
 import ProjectItem from "./project-item";
+import { AddRecord } from "./addRecord";
 
 const App = () => {
 	const [projectRecords,setProjectRecords]= useState([
-		{id:1,name:'how to custom a unix',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
-		{id:1,name:'how to custom a unix',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
-		{id:1,name:'how to custom a unix',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
-	])
+		{_id:1,name:'how to custom a unix',category:'important',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
+		{_id:2,name:'how to custom a unix',category:'daily',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
+		{_id:3,name:'how to custom a unix',category:'others',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
+  ])
+  
+  const addItem=(item)=>{
+    console.log(item);
+  }
   return (
     <Container>
       <h1>Book-Marker </h1>
+      <AddRecord addItem={addItem}></AddRecord>
       <Table striped bordered hover>
         <thead>
           <tr>
             <th>#</th>
             <th>Name</th>
+            <th>Category</th>
             <th>Description</th>
             <th>URL</th>
             <th>Date</th>
@@ -24,7 +31,7 @@ const App = () => {
         </thead>
         <tbody>
           {projectRecords.map((book)=>(
-			  <ProjectItem id={book.id}></ProjectItem>
+			  <ProjectItem key={book._id} record={book}></ProjectItem>
 		  ))}
           
         </tbody>
--- src/components/project-item.js
+++ src/components/project-item.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Badge, Button } from "react-bootstrap";
+import Moment from 'react-moment';
 
-const ProjectItem=(prop)=>{
-    return (
-        <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-          </tr>
-    )
-}
+const ProjectItem = ({
+  record: { _id, name, category, description, url, created },
+}) => {
+  const setVarient = () => {
+    if (category === "important") {
+      return "danger";
+    } else if (category === "daily") {
+      return "success";
+    }
+  };
+  return (
+    <tr className="h-100">
+      <td>{_id}</td>
+      <td className="align-middle">{name}</td>
+      <td className="align-middle">
+        <Badge className="d-flex justify-content-center" variant={setVarient()}>
+          {category.charAt(0).toUpperCase() + category.slice(1)}
+        </Badge>
+      </td>
+      {/* to capital the first letter of category name. */}
+      <td className="align-middle">{description}</td>
+      <td className="align-middle">{url}</td>
+      <td className="align-middle">
+        <Moment format="MMMM Do YYYY">{created}</Moment>
+      </td>
+      <td className="align-middle text-center">
+        <Button className="" variant="danger" size="sm">
+          X
+        </Button>
+      </td>
+    </tr>
+  );
+};
 
 export default ProjectItem;
