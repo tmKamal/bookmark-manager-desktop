a077f0f1d025cc105f5381a436d4c6e0c38bc43e
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-08-05 10:22:54 +0530

    dummy table creted for the main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React,{useState} from "react";
+import { Container, Table } from "react-bootstrap";
+import ProjectItem from "./project-item";
 
 const App = () => {
-	return (
-		<div className='app'>
-			<h1>Project Manger App</h1>
-			<p>This is a simple desktop appication to manage projects.</p>
-		</div>
-	)
-}
+	const [projectRecords,setProjectRecords]= useState([
+		{id:1,name:'how to custom a unix',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
+		{id:1,name:'how to custom a unix',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
+		{id:1,name:'how to custom a unix',description:'this is a smple text for you saved web content.',url:'https://github.com/tmKamal',created:new Date().toString()},
+	])
+  return (
+    <Container>
+      <h1>Book-Marker </h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>URL</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projectRecords.map((book)=>(
+			  <ProjectItem id={book.id}></ProjectItem>
+		  ))}
+          
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/project-item.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ProjectItem=(prop)=>{
+    return (
+        <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+    )
+}
+
+export default ProjectItem;
