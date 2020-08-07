ed177cb009c2e4d4796f9623f374ff97abd9c66b
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-08-07 11:32:36 +0530

    crud operation implementd

--- main.js
+++ main.js
@@ -1,85 +1,113 @@
-const path = require('path')
-const url = require('url')
-const { app, BrowserWindow } = require('electron');
-const connectDB = require('./config/db');
+const path = require("path");
+const url = require("url");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const connectDB = require("./config/db");
+const Bookmark = require("./models/bookmark");
 //connect to DB
 connectDB();
-let mainWindow
+let mainWindow;
 
-let isDev = false
+let isDev = false;
 
 if (
-	process.env.NODE_ENV !== undefined &&
-	process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV !== undefined &&
+  process.env.NODE_ENV === "development"
 ) {
-	isDev = true
+  isDev = true;
 }
 
 function createMainWindow() {
-	mainWindow = new BrowserWindow({
-		width: 1100,
-		height: 800,
-		show: false,
-		backgroundColor:'white',
-		icon: `${__dirname}/assets/icon.png`,
-		webPreferences: {
-			nodeIntegration: true,
-		},
-	})
+  mainWindow = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    show: false,
+    backgroundColor: "white",
+    icon: `${__dirname}/assets/icon.png`,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-	let indexPath
+  let indexPath;
 
-	if (isDev && process.argv.indexOf('--noDevServer') === -1) {
-		indexPath = url.format({
-			protocol: 'http:',
-			host: 'localhost:8080',
-			pathname: 'index.html',
-			slashes: true,
-		})
-	} else {
-		indexPath = url.format({
-			protocol: 'file:',
-			pathname: path.join(__dirname, 'dist', 'index.html'),
-			slashes: true,
-		})
-	}
+  if (isDev && process.argv.indexOf("--noDevServer") === -1) {
+    indexPath = url.format({
+      protocol: "http:",
+      host: "localhost:8080",
+      pathname: "index.html",
+      slashes: true,
+    });
+  } else {
+    indexPath = url.format({
+      protocol: "file:",
+      pathname: path.join(__dirname, "dist", "index.html"),
+      slashes: true,
+    });
+  }
 
-	mainWindow.loadURL(indexPath)
+  mainWindow.loadURL(indexPath);
 
-	// Don't show until we are ready and loaded
-	mainWindow.once('ready-to-show', () => {
-		mainWindow.show()
+  // Don't show until we are ready and loaded
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
 
-		// Open devtools if dev
-		if (isDev) {
-			const {
-				default: installExtension,
-				REACT_DEVELOPER_TOOLS,
-			} = require('electron-devtools-installer')
+    // Open devtools if dev
+    if (isDev) {
+      const {
+        default: installExtension,
+        REACT_DEVELOPER_TOOLS,
+      } = require("electron-devtools-installer");
 
-			installExtension(REACT_DEVELOPER_TOOLS).catch((err) =>
-				console.log('Error loading React DevTools: ', err)
-			)
-			mainWindow.webContents.openDevTools()
-		}
-	})
+      installExtension(REACT_DEVELOPER_TOOLS).catch((err) =>
+        console.log("Error loading React DevTools: ", err)
+      );
+      mainWindow.webContents.openDevTools();
+    }
+  });
 
-	mainWindow.on('closed', () => (mainWindow = null))
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.on('ready', createMainWindow)
+app.on("ready", createMainWindow); //app ready
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
+//fetch records
+ipcMain.on("records:load", sendRecords);
+async function sendRecords() {
+  const bookmarks = await Bookmark.find().sort({ created: 1 });
+  console.log(bookmarks);
+  mainWindow.webContents.send("records:get", JSON.stringify(bookmarks)); //send asynchronous msg to the renderer process via channel.
+} //do not use any arrow functions to created this type of methods
+
+//add records
+ipcMain.on("records:add", async (e, item) => {
+  try {
+    console.log(item);
+    await Bookmark.create(item);
+    sendRecords(); //calling the sendrecord function to fetch again and send to the fetchced values to the renderer process.
+  } catch (error) {
+    console.log(error);
+  }
+});
+//delete records
+ipcMain.on('records:delete',async (e,id)=>{
+	try {
+		await Bookmark.findOneAndDelete({_id:id});
+		sendRecords(); 
+	} catch (err) {
+		console.log(err);
 	}
 })
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
 
-app.on('activate', () => {
-	if (mainWindow === null) {
-		createMainWindow()
-	}
-})
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createMainWindow();
+  }
+});
 
 // Stop error
-app.allowRendererProcessReuse = true
+app.allowRendererProcessReuse = true;
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import { Container, Table, Alert } from "react-bootstrap";
 import ProjectItem from "./project-item";
 import { AddRecord } from "./addRecord";
+import { ipcRenderer } from "electron";
+
+
+
+
 
 const App = () => {
-	const [records,setRecords]= useState([
-		{_id:1,name:'how to custom a unix',category:'important',url:'https://github.com/tmKamal',created:new Date().toString()},
-		{_id:2,name:'how to custom a unix',category:'daily',url:'https://github.com/tmKamal',created:new Date().toString()},
-		{_id:3,name:'how to custom a unix',category:'others',url:'https://github.com/tmKamal',created:new Date().toString()},
-  ]);
+	const [records,setRecords]= useState([]);
 
   const [alert,setAlert]=useState({msg:'',varient:'',show:false});
-  
+
+  useEffect(() => {
+    ipcRenderer.send('records:load');
+    ipcRenderer.on('records:get',(e,recs)=>{
+      setRecords(JSON.parse(recs));
+    })//catch asynchronous msg which send from the main
+  }, [])
+
   const addItem=(item)=>{
     if(item.name==='' || item.category==='' || item.url===''){
       showAlert('Please fill out the all required feilds.',4000,'danger');
@@ -19,10 +27,10 @@ const App = () => {
     }
 
     console.log('im still i n')
-    item._id= Math.floor(Math.random( )*100);
+    /* item._id= Math.floor(Math.random( )*100);
     item.created=new Date().toString();
-    setRecords([...records,item]); 
-
+    setRecords([...records,item]);  */ //
+    ipcRenderer.send('records:add',item);
     showAlert('Bookmark has been saved');
   }
   const showAlert=(msg,time=4000,varient='success')=>{
@@ -43,11 +51,14 @@ const App = () => {
     
   }
   const deleteItemHandler=(_id)=>{
-    console.log('ima deleter')
-    setRecords(records.filter((book)=>
+    console.log('ima deleter');
+    ipcRenderer.send('records:delete',_id);
+
+    /* setRecords(records.filter((book)=>
       book._id !==_id
-    ));
+    )); */
   }
+  
   return (
     <Container>
       <h1>Book-Marker </h1>
